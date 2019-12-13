fb96604b5ddba2d2bdd0ccb4816bdf5367c867df
brad <brad@brad.tw>
2019-12-13 20:54:25 +0800

    2

--- app/src/main/java/tw/org/iii/android201913/MainActivity.java
+++ app/src/main/java/tw/org/iii/android201913/MainActivity.java
@@ -5,12 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
+public class MainActivity extends AppCompatActivity {
+    private GoogleMap googleMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new MyCallback());
+    }
+
+    private class MyCallback implements OnMapReadyCallback {
+
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+
+        }
     }
 
     public void zoomIn(View view) {
